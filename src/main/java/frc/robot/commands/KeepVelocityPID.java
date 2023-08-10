80268455493f0ca44873b60a916ba30f25edac1c
1HyperZ <omerzur06@gmail.com>
2023-08-10 14:25:50 +0300

    bill

--- src/main/java/frc/robot/commands/KeepVelocityPID.java
+++ src/main/java/frc/robot/commands/KeepVelocityPID.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import static frc.robot.Constants.ChassisConstants.*;
 import frc.robot.subsystems.ChassisSubsystem;
 
--- src/main/java/frc/robot/subsystems/ChassisSubsystem.java
+++ src/main/java/frc/robot/subsystems/ChassisSubsystem.java
@@ -5,7 +5,6 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import  static frc.robot.Constants.ChassisConstants.*;
 
 public class ChassisSubsystem extends SubsystemBase{
